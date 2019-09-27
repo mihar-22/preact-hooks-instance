fb2b8ddf366813b6e5e444c00b553e9584010ff4
Rahim Alwer <rahim_alwer@hotmail.com>
2019-09-27 14:51:17 +1000

    chore(types): add heading

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for Preact Hooks - Instance
+// Project: https://github.com/mihar-22/preact-hooks-instance
+// Definitions by: Rahim Alwer <https://github.com/mihar-22>
+
 declare function useInstance<T>(value: T): T extends (...args: any) => any ? ReturnType<T> : T;
 
 export as namespace useInstance;
